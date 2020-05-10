0f7a21dca27a0a57b1978f885a25b932ac0cc291
zekaio <zekailing@gmail.com>
2020-05-10 18:41:18 +0800

    背景预加载

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,47 @@
 </template>
 
 <script>
+import indexBg from "./assets/index_bg.png";
+import timeBg from "./assets/time_bg.png";
+import foodBg from "./assets/food_bg.png";
+import starBg from "./assets/star_bg.png";
+import marketBg from "./assets/market_bg.png";
+import { Loading } from "element-ui";
 export default {
   name: "app",
+  data: function() {
+    return {
+      imgs: [indexBg, timeBg, foodBg, starBg, marketBg],
+      count: 0,
+    };
+  },
+  methods: {
+    preload(loading) {
+      for (let i = 0; i < this.imgs.length; i++) {
+        let img = new Image();
+        img.onload = () => {
+          this.count++;
+          if (this.count == this.imgs.length) {
+            loading.close();
+          }
+        };
+        img.src = this.imgs[i];
+      }
+    },
+  },
   async mounted() {
     this.$router.push({
       path: "/index",
     });
+    let loadingInstance = Loading.service({ body: true, text: "加载中" });
+    this.preload(loadingInstance);
+    setTimeout(() => {
+      try {
+        loadingInstance.close();
+      } catch (e) {
+        console.log(e);
+      }
+    }, 3000);
   },
 };
 </script>
